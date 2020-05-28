8b674162cfa3d2c5c7bd2d7b0a6a86ac486cbcdf
meltingshine <meltingshine@gmail.com>
2020-05-28 21:43:31 +0900

    네이버 /카카오 링크

--- app/src/main/java/com/example/androidintent_20200528/MainActivity.kt
+++ app/src/main/java/com/example/androidintent_20200528/MainActivity.kt
@@ -41,5 +41,20 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+        naverLinkBtn.setOnClickListener {
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent(Intent.ACTION_VIEW, myUri)
+            startActivity(myIntent)
+        }
+
+        kakaoAppStoreBtn.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent(Intent.ACTION_VIEW,myUri)
+            startActivity(myIntent)
+
+        }
+
+
     }
 }
